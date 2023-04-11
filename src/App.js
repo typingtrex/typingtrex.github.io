f8923eb7d82987e38c95341ea0db42b2063f03ac
Ji Kim <jiyoung.kimmy@gmail.com>
2023-04-11 15:55:06 -0400

    more description

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-alert("it's working!!!");
+// alert("it's working!!!");
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello my name is Ji</p>
+        <p id="personal-description">I love to be creative and empathetic</p>
       </header>
     </div>
   );
