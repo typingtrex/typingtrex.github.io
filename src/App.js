d3f1f55e2abeed803ba50fdab4527f752d578e8c
Ji Kim <jiyoung.kimmy@gmail.com>
2023-10-30 21:38:48 -0400

    context about transitioning from healthcare to tech

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
       <header className="App-header">
         <p>Hello my name is Ji</p>
         <p id="personal-description">Welcome to my website</p>
+        <p>
+          I worked in healthcare prior to my switch to tech. One of my major
+          reasons for transitioning was to utilize the large community reach via
+          websites and apps.
+        </p>
       </header>
     </div>
   );
