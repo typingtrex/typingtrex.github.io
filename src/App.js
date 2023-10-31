2f3cfcdb942eb82220d844bb7372ef3d8548496f
Ji Kim <jiyoung.kimmy@gmail.com>
2023-10-31 13:29:24 -0400

    About me

--- src/App.js
+++ src/App.js
@@ -5,16 +5,53 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello my name is Ji</p>
-        <p id="personal-description">Welcome to my website</p>
+        <h2>Hello</h2>
+        <p id="personal-description">Nice to meet you</p>
+        <h4>ABOUT ME</h4>
         <p>
-          I worked in healthcare prior to my switch to tech. One of my major
-          reasons for transitioning was to utilize the large community reach via
-          websites and apps.
+          I'm Ji, and being a middle child I felt strongly about helping those
+          around me (AKA my siblings). Joining healthcare and working with
+          patients came naturally to me and was gratifying. However, my
+          perspective for "helping" shifted grately during COVID.
+        </p>
+        <p>
+          When NYC implemented a mandatory shutdown, I took over my parents'
+          laundromat. Opening and closing the store, I greeted each customer and
+          told them of the rules our family thought would best serve the
+          community:
+        </p>
+        <ul>
+          <li>Wearing a mask - free ones were provided.</li>
+          <li>
+            To leave the store after placing clothes in washer or dryer to avoid
+            crowds.
+          </li>
+          <li>To fold clothes at home, once again to avoid crowds.</li>
+        </ul>
+        <p>
+          Taking this initiative had some tough moments, where customers would
+          push back. However, by consistently explaining that these rules were
+          for us to keep everyone as safe as possible the community started to
+          change. Within 2 weeks customers and regulars would pop their heads in
+          to say,
+        </p>
+        <p>"Stay safe."</p>
+        <p>
+          Seeing the impact I can have in preventative vs treatment made me
+          consider switching to tech. I wanted to work in a space where I can
+          have a greater reach to uplift communities. Utilizing tech to help
+          others became a goal.
         </p>
         <p>
           I joined Grace Hopper at Full Stack Academy where I went from archaic
-          to tech literate and learned the joys of tech memes :)
+          to tech literate and learned the joys of tech memes :) My strong sense
+          of teamwork from years in NYC clinics brought my small team projects
+          to life. Currently I am focusing on my solo projects while devling
+          deeper into new technologies.
+        </p>
+        <p>
+          If you would like to connect please reach out via
+          <a href="https://www.linkedin.com/in/ji-young-kim/">LinkedIn</a>
         </p>
       </header>
     </div>
