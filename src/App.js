598d96d665f30b3368cfa983bee8d8b4e2f3addd
Ji Kim <jiyoung.kimmy@gmail.com>
2023-12-04 13:13:40 -0500

    adding photos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './components/Home';
+import NavBar from './components/NavBar';
 
 // alert("it's working!!!");
 function App() {
   return (
     <div className="App">
+      {/* <NavBar /> */}
       <Home />
     </div>
   );
